31dae2c30edd3dab2bfa38100a57fce9ed6f9fa9
praveenpriyashan <praveenpriyashan@gmail.com>
2024-10-04 07:53:11 +0530

    first commit

---
+++ service/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package com.ijse.hellospring.service;
+
+
+import com.ijse.hellospring.entity.User;
+import com.ijse.hellospring.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class  UserServiceImpl implements UserService {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(Long id, User user) {
+        User existUser = userRepository.findById(id).orElse(null);
+        if (existUser != null) {
+            existUser.setUsername(user.getUsername());
+            existUser.setPassword(user.getPassword());
+            existUser.setEmail(user.getEmail());
+            return userRepository.save(existUser);
+        }else {
+            return null;
+        }
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
